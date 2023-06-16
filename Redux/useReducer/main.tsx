8953a6202b1e7f2d805062eadf1160a602c960c9
Sagar Monga <sagar.monga3097@gmail.com>
2023-06-16 16:37:07 +0530

    add reducer

---
+++ Redux/useReducer/main.tsx
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react';
+
+// useReducer is a hook to use when you have complex state logic.
+// const [state, dispatch] = useReducer(reducer, initialValue)
+// dispatch is used to dispatch actions - dispatch(action)
+// the action is caught in the reducer. The reducer takes 2 params, state & action
+// state is the value of store, initialized by the initialValue parameter
+// action is an object which contains type and additional information, to be passed in dispatch.
+
+export function App(props) {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT': return state + 1;
+      case 'DECREMENT': return state - 1;
+      default: return state;
+    }
+  }
+  const [num, dispatch] = useReducer(reducer, 0);
+
+  const increment = () => {
+    dispatch({type: 'INCREMENT'});
+  }
+  const decrement = () => {
+    dispatch({type: 'DECREMENT'});
+  }
+  return (
+    <div className='App'>
+      <h1>Hello React.</h1>
+      <h2>{num}</h2>
+      <div>
+        <button onClick={increment}>Inc</button>
+        <button onClick={decrement}>Dec</button>
+      </div>
+    </div>
+  );
+}
