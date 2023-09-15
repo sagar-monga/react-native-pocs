5c2cefa6e5d8bc43ae138b8c7123acb1f7a50b85
Sagar Monga <sagar.monga3097@gmail.com>
2023-09-15 14:14:26 +0530

    Init app

---
+++ FirebaseRN/App.tsx
@@ -0,0 +1,12 @@
+import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'cyan'}}>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
