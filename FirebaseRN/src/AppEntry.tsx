2a8a2d5738c7e2cfd967c8516607bc9719b21f0a
Sagar Monga <sagar.monga3097@gmail.com>
2023-09-18 11:08:27 +0530

    refactor - update appstate logic to hook

--- FirebaseRN/src/AppEntry.tsx
+++ FirebaseRN/src/AppEntry.tsx
@@ -1,70 +1,12 @@
-import {AppState, StyleSheet} from 'react-native';
-import React, {isValidElement, useEffect, useRef, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import React from 'react';
 import ParentView from './components/ParentView';
 import HomeScreen from './screens/HomeScreen';
-import InactiveScreen from './screens/InactiveScreen';
-
-export const useAppStateChange = (
-  onAppBackgrounded = () => {},
-  onAppForegrounded = () => {},
-  onAppBlur = () => {},
-  onAppFocus = () => {},
-) => {
-  const appState = useRef(AppState.currentState);
-  const [isFocused, setIsFocused] = useState(true);
-  useEffect(() => {
-    const appStateSubscription = AppState.addEventListener(
-      'change',
-      nextState => {
-        if (
-          appState.current.match(/inactive|background/) &&
-          nextState.match(/active/)
-        ) {
-          setIsFocused(true);
-          onAppForegrounded();
-        } else if (
-          appState.current.match(/active/) &&
-          nextState.match(/inactive|background/)
-        ) {
-          setIsFocused(false);
-          onAppBackgrounded();
-        }
-
-        // setIsFocused(nextState === 'active');
-        appState.current = nextState;
-      },
-    );
-
-    // Works on recents button press on android but not on home button press
-    const blur = AppState.addEventListener('blur', () => {
-      onAppBlur();
-      setIsFocused(false);
-    });
-
-    const focus = AppState.addEventListener('focus', () => {
-      onAppFocus();
-      setIsFocused(true);
-    });
-
-    return () => {
-      appStateSubscription.remove();
-      blur.remove();
-      focus.remove();
-    };
-  }, []);
-
-  return {isFocused};
-};
+import LoginScreen from './screens/LoginScreen';
 
 const AppEntry = () => {
-  const {isFocused} = useAppStateChange();
-  return (
-    <ParentView>
-      {/* <HomeScreen />
-      {!isFocused && <InactiveScreen />} */}
-      {isFocused ? <HomeScreen /> : <InactiveScreen />}
-    </ParentView>
-  );
+  const auth = false;
+  return <ParentView>{auth ? <HomeScreen /> : <LoginScreen />}</ParentView>;
 };
 
 export default AppEntry;
