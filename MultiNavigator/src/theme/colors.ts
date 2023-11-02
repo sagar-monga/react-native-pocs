dc6bec7c639e31ac89b8c5b56b160867b9d703e3
Sagar Monga <sagar.monga3097@gmail.com>
2023-11-02 14:29:58 +0530

    Update colors

--- MultiNavigator/src/theme/colors.ts
+++ MultiNavigator/src/theme/colors.ts
@@ -1,7 +1,7 @@
 export const Colors = {
   black: '#1a1a1a', // primary
-  grape: '#6f08af', // secondary
+  grape: '#9a52c7', // secondary
   white: '#fbfbfb', // text
   bittersweet: '#f26157', // tertiary
-  goldenRod: '#9d8420', // 
+  goldenRod: '#9d8420', //
 };
