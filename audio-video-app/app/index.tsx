e89d89e4c4139e94abf5f354e424f76bb20499ce
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-09 15:59:12 +0530

    feat: add routes to enum

--- audio-video-app/app/index.tsx
+++ audio-video-app/app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Button, Pressable, Text, View } from "react-native";
+import { Routes } from "./routes/Routes";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello</Text>
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text style={{ padding: 40 }}>Hello</Text>
+			<Pressable onPress={() => {}}>
+				<Link
+					href={Routes.Content}
+					style={{
+						backgroundColor: "blue",
+						color: "white",
+						paddingHorizontal: 15,
+						paddingVertical: 10,
+						borderRadius: 50,
+					}}
+				>
+					View Content
+				</Link>
+			</Pressable>
+		</View>
+	);
 }
